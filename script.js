c59bee3ca29e55d8b918d6e27335b316e4e90a7d
dstille <77865578+dstille@users.noreply.github.com>
2024-04-04 20:39:57 +0000

    added json stuff

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ function printMadLib() {
     'nounObj', 'verbTransPast1', 'nounPlural2', 'nounPlural3', 'verbTransPast2']
   pos.forEach(getInput)
   document.getElementById("result").innerHTML = story
+  console.log("story: " + story);
+  storyData[story] = story;
+  var storyJSON = JSON.stringify(storyData);
+  console.log("storyJSON: " + storyJSON);
+  return storyJSON;
 }
 
 function getInput(item, index, array) {
   word = document.getElementById(item).value;
-  story = story.replace("___", "<u>" + validate(word, item) + "</u>");
+  storyData[item] = word;
+  console.log("word: " + word);
+  story = story.replace("___", "<u>" + word + "</u>");
 }
 
 function validate(word, item) {
@@ -25,3 +32,6 @@ function validate(word, item) {
 
 var story = '"___!", the ___ gentleman said,<br>"What are you doing with those ___? You cant do that!<br>You are in ___ trouble there, Buddy!<br>That could get you ___ years in the lock-up." <br>He then ___ grabbed his ___ and ___ it.<br>His scooter was kicking up ___ as he sped away to contact the ___.<br>Somebody was going get ___.';
 
+var storyData = {
+  timestamp: Date.now()
+}
